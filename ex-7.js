ed2c6fd6139a425c6739e6d3926f3b43cdcfad41
CharlieHii <charlie_chaper@hotmail.com>
2024-05-21 16:33:06 +0700

    feat(*.js): upgrade palindrome

--- ex-7.js
+++ ex-7.js
@@ -7,3 +7,29 @@ function isPalindrome(string) {
 console.log(isPalindrome("reviver")); // true
 console.log(isPalindrome("บวบ")); // true
 console.log(isPalindrome("deliver")); // false
+
+const isPalindromeMai = (string) => {
+  let indexStart = 0;
+  let indexStop = string.length - 1;
+  string = string.toLowerCase();
+  let notCountSet = [" ", ","];
+  while (indexStart < indexStop) {
+    while (notCountSet.includes(string[indexStart])) {
+      indexStart += 1;
+    }
+    while (notCountSet.includes(string[indexStop])) {
+      indexStop -= 1;
+    }
+    if (string[indexStart] !== string[indexStop]) {
+      return false;
+    } else {
+      indexStart += 1;
+      indexStop -= 1;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindromeMai("reviver")); // true
+console.log(isPalindromeMai("บวบ")); // true
+console.log(isPalindromeMai("deliver")); // false
